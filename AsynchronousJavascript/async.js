d8be281842ecfa73e59676cfd3e03a4cd1be6046
Md Asibul Hasan Shanto <48201478+asibulhasanshanto@users.noreply.github.com>
2022-12-12 13:15:07 +0600

    asynchoronos js done

--- AsynchronousJavascript/async.js
+++ AsynchronousJavascript/async.js
@@ -1,4 +1,3 @@
-
 var asyncBasic = function () {
   // synchoronous code
   console.log("1");
@@ -26,37 +25,80 @@ var asyncBasic = function () {
 // asyncBasic();
 
 // creating simple promise and using it
-var simplePromise = function(){
-    const lotteryPromise = new Promise((resolve,reject)=>{
-        if(Math.random()>=0.5){
-            resolve('You Win');
-        }else{
-            reject(new Error('You Lose'));
-        }
-    })
+var simplePromise = function () {
+  const lotteryPromise = new Promise((resolve, reject) => {
+    if (Math.random() >= 0.5) {
+      resolve("You Win");
+    } else {
+      reject(new Error("You Lose"));
+    }
+  });
 
-    lotteryPromise.then(res=>console.log(res)).catch(err=>console.error(err));
-}
+  lotteryPromise
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
 
-simplePromise();
+// simplePromise();
 
 // promisify setTimeout
-var promisifySetTimeout = function(){
-    const wait = function(seconds){
-        return new Promise(resolve=>{
-            setTimeout(resolve,seconds*1000);
-        })
-    }
+var promisifySetTimeout = function () {
+  const wait = function (seconds) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, seconds * 1000);
+    });
+  };
 
-    wait(2).then(()=>{
-        console.log('I waited for 2 seconds');
-        return wait(1);
-    }).then(()=>{
-        console.log('I waited for 1 second');
-        return wait(3);
-    }).then(()=>{
-        console.log('I waited for 3 second');
+  wait(2)
+    .then(() => {
+      console.log("I waited for 2 seconds");
+      return wait(1);
     })
-}
+    .then(() => {
+      console.log("I waited for 1 second");
+      return wait(3);
+    })
+    .then(() => {
+      console.log("I waited for 3 second");
+    });
+};
 
-promisifySetTimeout();
+// promisifySetTimeout();
+
+const asyncAwait = function () {
+  // Returning Values from Async Functions
+  const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+      resolve({latitude: '88.612097', longitude: '24.374403'});
+    });
+  };
+  const whereAmI = async function () {
+    try {
+      // Geolocation
+      const pos = await getPosition();
+      const { latitude: lat, longitude: lng } = pos;
+      // Reverse geocoding
+      const resGeo = await fetch(
+        `https://geocode.xyz/${lat},${lng}?geoit=json`
+      );
+      if (!resGeo.ok) throw new Error("Problem getting location data");
+      const dataGeo = await resGeo.json();
+      // Country data
+      const res = await fetch(
+        `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+      );
+      if (!resGeo.ok) throw new Error("Problem getting country");
+      const data = await res.json();
+      return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+    } catch (err) {
+      console.error(`${err} 💥`);
+      // Reject promise returned from async function
+      throw err;
+    }
+  };
+  console.log("1: Will get location");
+  const city = whereAmI();
+  console.log(city);
+};
+
+asyncAwait();
--- AsynchronousJavascript/async.md
+++ AsynchronousJavascript/async.md
@@ -96,3 +96,78 @@ I waited for 3 second
 */
 
 ```
+
+## How asynchronous code works in javascript
+
+![Asynchronous code](/AsynchronousJavascript/asynchronous_js_behind.PNG)
+
+## Using async await pattern to write asynchronous code
+
+```javascript
+// Returning Values from Async Functions
+  const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+      resolve({latitude: '88.612097', longitude: '24.374403'});
+    });
+  };
+  const whereAmI = async function () {
+    try {
+      // Geolocation
+      const pos = await getPosition();
+      const { latitude: lat, longitude: lng } = pos;
+      // Reverse geocoding
+      const resGeo = await fetch(
+        `https://geocode.xyz/${lat},${lng}?geoit=json`
+      );
+      if (!resGeo.ok) throw new Error("Problem getting location data");
+      const dataGeo = await resGeo.json();
+      // Country data
+      const res = await fetch(
+        `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+      );
+      if (!resGeo.ok) throw new Error("Problem getting country");
+      const data = await res.json();
+      return `You are in ${dataGeo.city}, ${dataGeo.country}`;
+    } catch (err) {
+      console.error(`${err} 💥`);
+      // Reject promise returned from async function
+      throw err;
+    }
+  };
+  console.log("1: Will get location");
+  const city = whereAmI();
+  console.log(city);
+
+  ```
+
+  ## Runnung promises in parallel
+  
+  ```javascript
+    // Running Promises in Parallel
+      const get3Countries = async function (c1, c2, c3) {
+        try {
+          // const [data1] = await getJSON(
+          //   `https://restcountries.eu/rest/v2/name/${c1}`
+          // );
+          // const [data2] = await getJSON(
+          //   `https://restcountries.eu/rest/v2/name/${c2}`
+          // );
+          // const [data3] = await getJSON(
+          //   `https://restcountries.eu/rest/v2/name/${c3}`
+          // );
+          // console.log([data1.capital, data2.capital, data3.capital]);
+          const data = await Promise.all([
+            getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+            getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+            getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+          ]);
+          console.log(data.map(d => d[0].capital));
+        } catch (err) {
+          console.error(err);
+        }
+      };
+      get3Countries('portugal', 'canada', 'tanzania');
+
+```
+  
+  
